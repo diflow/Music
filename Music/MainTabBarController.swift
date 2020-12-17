9b226159f7310b9ad481d483f04e4407cbd3891b
diflow <dvornyk.ivan@gmail.com>
2020-12-18 01:14:00 +0200

    Alamofire added

---
+++ Music/MainTabBarController.swift
@@ -0,0 +1,34 @@
+//
+//  MainTabBarController.swift
+//  Music
+//
+//  Created by ivan on 17.12.2020.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+        tabBar.tintColor = #colorLiteral(red: 1, green: 0, blue: 0.3764705882, alpha: 1)
+        
+        viewControllers = [
+            generationViewController(rootViewCOntroller: SearchViewController(), image: #imageLiteral(resourceName: "ios10-apple-music-search-5nav-icon-1"), title: "Search"),
+            generationViewController(rootViewCOntroller: ViewController(), image: #imageLiteral(resourceName: "ios10-apple-music-library-5nav-icon"), title: "Library")
+        ]
+    }
+    
+    private func generationViewController(rootViewCOntroller: UIViewController, image: UIImage, title: String) -> UIViewController {
+        
+        let navigationVC = UINavigationController(rootViewController: rootViewCOntroller)
+        navigationVC.tabBarItem.image = image
+        navigationVC.tabBarItem.title = title
+        rootViewCOntroller.navigationItem.title = title
+        navigationVC.navigationBar.prefersLargeTitles = true
+        return navigationVC
+    }
+}
---
+++ Music/SearchViewController.swift
@@ -0,0 +1,87 @@
+//
+//  SearchViewController.swift
+//  Music
+//
+//  Created by ivan on 17.12.2020.
+//
+
+import UIKit
+import Alamofire
+
+struct TrackModel {
+    var trackName: String
+    var artistName: String
+}
+
+class SearchViewController: UITableViewController {
+    
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    var tracks = [Track]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.backgroundColor = .white
+        
+        setupSearchBar()
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+    }
+    
+    private func setupSearchBar() {
+        
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.searchBar.delegate = self
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tracks.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        
+        let track = tracks[indexPath.row]
+        cell.textLabel?.text = "\(track.trackName)\n\(track.artistName)"
+        cell.textLabel?.numberOfLines = 2
+        
+        return cell
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
+        
+        let url = "https://itunes.apple.com/search?term=\(searchText)"
+        
+        AF.request(url).response { (dataResponse) in
+            if let error = dataResponse.error {
+                print(error)
+                return
+            }
+            
+            guard let data = dataResponse.data else { return }
+            
+            let decoder = JSONDecoder()
+            do {
+                let objects = try decoder.decode(SearchResponse.self, from: data)
+                print(objects)
+                self.tracks = objects.results
+                self.tableView.reloadData()
+                
+            } catch let error {
+                print(error)
+            }
+            
+//            let someString = String(data: data, encoding: .utf8)
+//            print(someString ?? "")
+        }
+        
+    }
+}
