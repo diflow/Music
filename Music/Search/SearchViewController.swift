90bcba52022edea786d8e539bba0a8fb66adcbf5
diflow <dvornyk.ivan@gmail.com>
2020-12-21 01:28:16 +0200

    next Track added

--- Music/Search/SearchViewController.swift
+++ Music/Search/SearchViewController.swift
@@ -106,6 +106,7 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
         let window = UIApplication.shared.keyWindow
         let trackDetailView = Bundle.main.loadNibNamed("TrackDetailView", owner: self, options: nil)?.first as! TrackDetailView
         trackDetailView.set(viewModel: cellViewModel)
+        trackDetailView.delegate = self
         window?.addSubview(trackDetailView)
     }
     
@@ -135,3 +136,38 @@ extension SearchViewController: UISearchBarDelegate {
     }
 }
 
+extension SearchViewController: TrackMovingDelegate {
+    
+    private func getTrack(isForwardTrack: Bool) -> SearchViewModel.Cell? {
+        
+        guard let indexPath = table.indexPathForSelectedRow else { return nil }
+        table.deselectRow(at: indexPath, animated: true)
+        var nextIndexPath: IndexPath!
+        if isForwardTrack {
+            nextIndexPath = IndexPath(row: indexPath.row + 1, section: indexPath.section)
+            if nextIndexPath.row == searchViewModel.cells.count {
+                nextIndexPath.row = 0
+            }
+        } else {
+            nextIndexPath = IndexPath(row: indexPath.row - 1, section: indexPath.section)
+            if nextIndexPath.row == -1 {
+                nextIndexPath.row = searchViewModel.cells.count - 1
+            }
+        }
+        table.selectRow(at: nextIndexPath, animated: true, scrollPosition: .none)
+        let cellViewModel = searchViewModel.cells[nextIndexPath.row]
+        return cellViewModel
+    }
+    
+    func moveBackForPreviousTrack() -> SearchViewModel.Cell? {
+        print("moveBackForPreviousTrack")
+        return getTrack(isForwardTrack: false)
+    }
+    
+    func moveForwardForPreviousTrack() -> SearchViewModel.Cell? {
+        print("moveForwardForPreviousTrack")
+        return getTrack(isForwardTrack: true)
+    }
+    
+    
+}
--- Music/Search/TrackDetailView/TrackDetailView.swift
+++ Music/Search/TrackDetailView/TrackDetailView.swift
@@ -9,6 +9,11 @@ import UIKit
 import SDWebImage
 import AVKit
 
+protocol TrackMovingDelegate: class {
+    func moveBackForPreviousTrack() -> SearchViewModel.Cell?
+    func moveForwardForPreviousTrack() -> SearchViewModel.Cell?
+}
+
 class TrackDetailView: UITableViewCell {
     
     @IBOutlet weak var trackImageView: UIImageView!
@@ -25,6 +30,9 @@ class TrackDetailView: UITableViewCell {
         avPlayer.automaticallyWaitsToMinimizeStalling = false
         return avPlayer
     }()
+    
+    weak var delegate: TrackMovingDelegate?
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         
@@ -35,12 +43,15 @@ class TrackDetailView: UITableViewCell {
         
     }
     
+    // MARK: - Setup
+    
     func set(viewModel: SearchViewModel.Cell) {
         
         trackTitleLabel.text = viewModel.trackName
         autorTitleLabel.text = viewModel.artistName
         playTrack(previewUrl: viewModel.previewUrl)
         monitorStarTime()
+        observerLayerCurrentTime()
         let string600 = viewModel.iconUrlString?.replacingOccurrences(of: "100x100", with: "600x600")
         guard let url = URL(string: string600 ?? "") else { return }
         trackImageView.sd_setImage(with: url, completed: nil)
@@ -53,6 +64,8 @@ class TrackDetailView: UITableViewCell {
         player.play()
     }
     
+    // MARK: - Time setup
+    
     private func monitorStarTime() {
         let time = CMTimeMake(value: 1, timescale: 3)
         let times = [NSValue(time: time)]
@@ -60,6 +73,28 @@ class TrackDetailView: UITableViewCell {
             self?.enlargeTrackImageView()
         }
     }
+    
+    private func observerLayerCurrentTime() {
+        
+        let interval = CMTimeMake(value: 1, timescale: 2)
+        player.addPeriodicTimeObserver(forInterval: interval, queue: .main) { [weak self] (time) in
+            self?.currectTimeLabel.text = time.toDisplayString()
+            
+            let durationTime = self?.player.currentItem?.duration
+            let currentDurationText = ((durationTime ?? CMTimeMake(value: 1, timescale: 1)) - time).toDisplayString()
+            self?.durationLabel.text = "-\(currentDurationText)"
+            self?.updateCurrentTimeSlider()
+        }
+    }
+    
+    private func updateCurrentTimeSlider() {
+        let currentTimeSeconds = CMTimeGetSeconds(player.currentTime())
+        let durationSeconds = CMTimeGetSeconds(player.currentItem?.duration ?? CMTimeMake(value: 1, timescale: 1))
+        let percentage = currentTimeSeconds / durationSeconds
+        self.currectTimeSlider.value = Float(percentage)
+    }
+    
+    // MARK: - Animation
     
     private func enlargeTrackImageView() {
         UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: .curveEaseInOut, animations: {
@@ -74,17 +109,35 @@ class TrackDetailView: UITableViewCell {
         }, completion: nil)
     }
     
+    // MARK: - IBAction
+    
     @IBAction func handleCurrentTimeSlider(_ sender: Any) {
+        
+        let percentage = currectTimeSlider.value
+        guard let duration = player.currentItem?.duration else { return }
+        let durationInSeconds = CMTimeGetSeconds(duration)
+        let seekTimeinSeconds = Float64(percentage) * durationInSeconds
+        let seekTime = CMTimeMakeWithSeconds(seekTimeinSeconds, preferredTimescale: 1)
+        player.seek(to: seekTime)
     }
     @IBAction func handleVolumeSlider(_ sender: Any) {
+        player.volume = volumeSlider.value
     }
     
     @IBAction func dragDownButtonTapped(_ sender: Any) {
         self.removeFromSuperview()
     }
+    
     @IBAction func previousTrack(_ sender: Any) {
+        let cellViewModel = delegate?.moveBackForPreviousTrack()
+        guard let cellInfo = cellViewModel else { return }
+        self.set(viewModel: cellInfo)
     }
+    
     @IBAction func nextTrack(_ sender: Any) {
+        let cellViewModel = delegate?.moveForwardForPreviousTrack()
+        guard let cellInfo = cellViewModel else { return }
+        self.set(viewModel: cellInfo)
     }
     @IBAction func playPauseAction(_ sender: Any) {
         if player.timeControlStatus == .paused {
